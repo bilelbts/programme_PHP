eb723a12e4bfa2e5c78da12ba7b3bdcd729c11e5
Bilal Hadjersi <150812204+bilelbts@users.noreply.github.com>
2025-07-18 05:22:23 +0200

    Update mysql.php

--- config/mysql.php
+++ config/mysql.php
@@ -1,7 +1,7 @@
 <?php
 
-const MYSQL_HOST = 'localhost';
-const MYSQL_PORT = 3306;
+const MYSQL_HOST = 'yours';
+const MYSQL_PORT = yours;
 const MYSQL_NAME = 'partage_de_recettes';
-const MYSQL_USER = 'root';
-const MYSQL_PASSWORD = '';
+const MYSQL_USER = 'yours';
+const MYSQL_PASSWORD = yours;
